36259adcd7ee307c5422e437dba70b560a4f46a1
Gady <gady@margalit.ws>
2022-09-03 14:33:34 +0300

    fix db ram name list

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def hello():
     if name != "":
         name_list.append(name)    
 
-    name_str = "".split(name_list, "\n")
+    name_str = "\n".join(name_list)
     return "Hello users\n" + name_str
 
 if __name__ == '__main__':
