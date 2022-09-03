2f8940f79cedd0b552250777c8213745c6f02798
Gady <gady@margalit.ws>
2022-09-03 14:06:07 +0300

    podtwo

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hellfire4 Local!"
+    return "Hellfire4 2 pods!"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
